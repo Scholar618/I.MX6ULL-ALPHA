f542caad353b0b47857544f4e9f764fb614c0bea
Scholar618 <107455043+Scholar618@users.noreply.github.com>
2023-08-13 13:21:01 +0800

    Create CC.h

---
+++ NXP官方SDK使用/CC.h
@@ -0,0 +1,25 @@
+#ifndef __CC_H
+#define __CC_H
+
+#define __I     volatile
+#define __O     volatile
+#define __IO    volatile
+
+typedef signed char     int8_t;
+typedef signed short    int16_t;
+typedef signed int      int32_t;
+typedef signed char     uint8_t;
+typedef signed short    uint16_t;
+typedef signed int      uint32_t;
+typedef signed long long     uint64_t;
+
+typedef signed char         s8;
+typedef signed short        s16;
+typedef signed int          s32;
+typedef signed long long    s64;
+typedef signed char         u8;
+typedef signed short        u16;
+typedef signed int          u32;
+typedef signed long long    u64;
+
+#endif // !__CC_H
